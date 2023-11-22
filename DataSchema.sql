b812482c5e0c62408e6f838430117ffb1e7a60bf
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2023-11-22 17:44:41 +0100

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,102 +1,98 @@
 CREATE TABLE Users (
     Userno INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    region VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255) NOT NULL
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Region VARCHAR(255) NOT NULL,
+    Phone_Number VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE TemperatureRequests (
     TemperatureRequestno INT NOT NULL,
-    Userno INT NOT NULL, 
-    region VARCHAR(255) NOT NULL,
-    info VARCHAR(255) NOT NULL
+    Userno INT NOT NULL,
+    Region VARCHAR(255) NOT NULL,
+    Info VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE TemperatureAnswers (
     TemperatureAnswerno INT NOT NULL,
     TemperatureRequestno INT NOT NULL,
     Windyno INT NOT NULL,
-    answer INT NOT NULL,
-    "date" VARCHAR(255) NOT NULL
+    Answer INT NOT NULL,
+    Date VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Windy (
     Windyno INT NOT NULL,
-    region VARCHAR(255) NOT NULL,
+    Region VARCHAR(255) NOT NULL,
     WheatherState VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE HealthConsultationRequests (
     HealthConsultationRequestno INT NOT NULL,
     Userno INT NOT NULL,
-    "date" DATE NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    userInfo VARCHAR(255) NOT NULL
+    Date DATE NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    UserInfo VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Doctors (
     Doctorno INT NOT NULL,
     HealthConsultationRequestno INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    speciality VARCHAR(255) NOT NULL,
-    phoneNumber VARCHAR(255) NOT NULL
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Speciality VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(255) NOT NULL
 );
 
 /* Первинні ключі */
-ALTER TABLE Users ADD CONSTRAINT user_pk
-    PRIMARY KEY (Userno);
-	
-ALTER TABLE TemperatureRequests ADD CONSTRAINT TemperatureRequests_pk
-    PRIMARY KEY (TemperatureRequestno);
+ALTER TABLE Users ADD CONSTRAINT User_Pk
+PRIMARY KEY (Userno);
 
-ALTER TABLE TemperatureAnswers ADD CONSTRAINT TemperatureAnswers_pk
-    PRIMARY KEY (TemperatureAnswerno);
+ALTER TABLE TemperatureRequests ADD CONSTRAINT TemperatureRequests_Pk
+PRIMARY KEY (TemperatureRequestno);
 
-ALTER TABLE Windy ADD CONSTRAINT Windy_pk
-    PRIMARY KEY (Windyno);
+ALTER TABLE TemperatureAnswers ADD CONSTRAINT TemperatureAnswers_Pk
+PRIMARY KEY (TemperatureAnswerno);
 
-ALTER TABLE Doctors ADD CONSTRAINT Doctors_pk
-    PRIMARY KEY (Doctorno);
+ALTER TABLE Windy ADD CONSTRAINT Windy_Pk
+PRIMARY KEY (Windyno);
 
-ALTER TABLE HealthConsultationRequests ADD CONSTRAINT HealthConsultationRequests_pk
-    PRIMARY KEY (HealthConsultationRequestno);
+ALTER TABLE Doctors ADD CONSTRAINT Doctors_Pk
+PRIMARY KEY (Doctorno);
+
+ALTER TABLE HealthConsultationRequests ADD CONSTRAINT HealthConsultationRequests_Pk
+PRIMARY KEY (HealthConsultationRequestno);
 
 /* Зовнішні ключі */
-ALTER TABLE TemperatureRequests ADD CONSTRAINT Userno_fk
-    FOREIGN KEY (Userno) REFERENCES Users(Userno);
+ALTER TABLE TemperatureRequests ADD CONSTRAINT Userno_Fk
+FOREIGN KEY (Userno) REFERENCES Users (Userno);
 
-ALTER TABLE TemperatureAnswers ADD CONSTRAINT TemperatureRequestno_fk
-    FOREIGN KEY (TemperatureRequestno) REFERENCES TemperatureRequests(TemperatureRequestno);
+ALTER TABLE TemperatureAnswers ADD CONSTRAINT TemperatureRequestno_Fk
+FOREIGN KEY (TemperatureRequestno) REFERENCES TemperatureRequests (
+    TemperatureRequestno
+);
 
-ALTER TABLE TemperatureAnswers ADD CONSTRAINT Windyno_fk
-    FOREIGN KEY (Windyno) REFERENCES Windy(Windyno);
+ALTER TABLE TemperatureAnswers ADD CONSTRAINT Windyno_Fk
+FOREIGN KEY (Windyno) REFERENCES Windy (Windyno);
 
-ALTER TABLE HealthConsultationRequests ADD CONSTRAINT Userno_fk1
-    FOREIGN KEY (Userno) REFERENCES Users(Userno);
+ALTER TABLE HealthConsultationRequests ADD CONSTRAINT Userno_Fk1
+FOREIGN KEY (Userno) REFERENCES Users (Userno);
 
-ALTER TABLE Doctors ADD CONSTRAINT HealthConsultationRequestno_fk
-    FOREIGN KEY (HealthConsultationRequestno) REFERENCES HealthConsultationRequests(HealthConsultationRequestno);
+ALTER TABLE Doctors ADD CONSTRAINT HealthConsultationRequestno_Fk
+FOREIGN KEY (
+    HealthConsultationRequestno
+) REFERENCES HealthConsultationRequests (HealthConsultationRequestno);
 
 /* Обмеження змісту атрибутів таблиць */
 
 ALTER TABLE Users
-ADD CONSTRAINT valid_email_format
-CHECK (REGEXP_LIKE(email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$'));
+ADD CONSTRAINT VALID_EMAIL_FORMAT
+CHECK (REGEXP_LIKE(Email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$'));
 
 ALTER TABLE Users
-ADD CONSTRAINT valid_phone_format
-CHECK (REGEXP_LIKE(phone_number, '^[0-9]{10}$'));
+ADD CONSTRAINT VALID_PHONE_FORMAT
+CHECK (REGEXP_LIKE(Phone_Number, '^[0-9]{10}$'));
 
 ALTER TABLE HealthConsultationRequests
-ADD CONSTRAINT request_date_in_past
-CHECK ("date" <= TRUNC(SYSDATE));
-
-
-
-ALTER TABLE AirConditionStateRequests
-ADD CONSTRAINT valid_location_format
-CHECK (region ~* '^[A-Za-z ]+$');
-ALTER TABLE Doctor ADD CONSTRAINT HealthConsultationRequestno_fk
-    FOREIGN KEY (HealthConsultationRequestno) REFERENCES HealthConsultationRequests(HealthConsultationRequestno);
+ADD CONSTRAINT REQUEST_DATE_IN_PAST
+CHECK (Date <= TRUNC(SYSDATE));
