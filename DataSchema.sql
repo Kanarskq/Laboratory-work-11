317eae2a1d1a7a330c94877308a53cc7b50925b6
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2023-11-22 19:02:01 +0100

    Update 8 DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Users (
-    Userno INT NOT NULL,
+    Userno INT PRIMARY KEY,
     UsersName VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
     Region VARCHAR(255) NOT NULL,
@@ -7,14 +7,14 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE TemperatureRequests (
-    TemperatureRequestno INT NOT NULL,
+    TemperatureRequestno INT PRIMARY KEY,
     Userno INT NOT NULL,
     Region VARCHAR(255) NOT NULL,
     Info VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE TemperatureAnswers (
-    TemperatureAnswerno INT NOT NULL,
+    TemperatureAnswerno INT PRIMARY KEY,
     TemperatureRequestno INT NOT NULL,
     Windyno INT NOT NULL,
     Answer INT NOT NULL,
@@ -22,13 +22,13 @@ CREATE TABLE TemperatureAnswers (
 );
 
 CREATE TABLE Windy (
-    Windyno INT NOT NULL,
+    Windyno INT PRIMARY KEY,
     Region VARCHAR(255) NOT NULL,
     WheatherState VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE HealthConsultationRequests (
-    HealthConsultationRequestno INT NOT NULL,
+    HealthConsultationRequestno INT PRIMARY KEY,
     Userno INT NOT NULL,
     RequestDate DATE NOT NULL,
     Description VARCHAR(255) NOT NULL,
@@ -36,26 +36,13 @@ CREATE TABLE HealthConsultationRequests (
 );
 
 CREATE TABLE Doctors (
-    Doctorno INT NOT NULL,
+    Doctorno INT PRIMARY KEY,
     HealthConsultationRequestno INT NOT NULL,
     DoctorsName VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
     Speciality VARCHAR(255) NOT NULL,
     PhoneNumber VARCHAR(255) NOT NULL
 );
-/* Первинні ключі */
-ALTER TABLE Users ADD CONSTRAINT UserPk PRIMARY KEY (Userno);
-
-
-ALTER TABLE TemperatureRequests ADD CONSTRAINT TemperatureRequestsPk PRIMARY KEY (TemperatureRequestno);
-
-ALTER TABLE TemperatureAnswers ADD CONSTRAINT TemperatureAnswersPk PRIMARY KEY (TemperatureAnswerno);
-
-ALTER TABLE Windy ADD CONSTRAINT WindyPk PRIMARY KEY (Windyno);
-
-ALTER TABLE Doctors ADD CONSTRAINT DoctorsPk PRIMARY KEY (Doctorno);
-
-ALTER TABLE HealthConsultationRequests ADD CONSTRAINT HealthConsultationRequestsPk PRIMARY KEY (HealthConsultationRequestno);
 
 /* Зовнішні ключі */
 ALTER TABLE TemperatureRequests ADD CONSTRAINT UsernoFk FOREIGN KEY (Userno) REFERENCES Users(Userno);
