a4d8dc7b0ca54e218ec9323f61cf34c2ca5dac50
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2023-11-23 21:45:51 +0100

    Update 4 ProgramCodeModified.sql

--- ProgramCodeModified.sql
+++ ProgramCodeModified.sql
@@ -1,7 +1,6 @@
 sqlplus ${{ secrets.LOGIN }}/${{ secrets.PASSWORD }}@91.219.60.189:1521/XEPDB1
 
 CREATE OR REPLACE FUNCTION GetTemperature (user IN T_USERS, region IN VARCHAR2) RETURN INTEGER IS
-    VTemperatureState VARCHAR2(25);
     VResult INTEGER := 1;
 
 /
