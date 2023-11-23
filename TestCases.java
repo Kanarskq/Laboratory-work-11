f565c859b1eb7d3de4ba1448a430841dde792364
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2023-11-23 22:32:32 +0100

    Update TestCases.java

--- TestCases.java
+++ TestCases.java
@@ -3,46 +3,97 @@ import oracle.jdbc.driver.*;
 
 class TestCases {
     public static void main(final String[] args) {
-		int testCaseResult = 0; // 0 = Passed -1 = Failed
+        int testCaseResult = 0; // 0 = Passed -1 = Failed
         try {
             // load Oracle JDBC-driver
-            DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            
             // open connect to DB
-            Connection con=DriverManager.getConnection(  
-                                  "jdbc:oracle:thin:@91.219.60.189:1521/XEPDB1",
-			args[0], // get login as 1th command line parameter
-			args[1]); // get password as 2nd command line parameter 
-            // create template string with PL/SQL-function "add_user"
-			CallableStatement cstmt1 = con.prepareCall("{? = call add_user(?,?)}");
-			cstmt1.registerOutParameter(1,Types.NUMERIC);		
-			System.out.print("TC1 ( user 'user1' is not exists; add_user('user1','a12A345678#') = 1 ) := ");
-            Statement cstmt2 = con.createStatement();
-			cstmt2.executeUpdate("DELETE FROM Users WHERE uname = upper('user1')");
-            // init template variables
-			cstmt1.setString(2, "user1");
-			cstmt1.setString(3, "a12A345678#");
-			// execute query
-			cstmt1.executeUpdate();
-			// analize TestCase-result
-            if (cstmt1.getInt(1) == 1) System.out.println("Passed");
-	    else { 
-	        System.out.println("Failed");
-			testCaseResult = -1;
-	    }
-    	    System.out.print("TC2 ( user 'user1' is exists; add_user('user1','a12A345678#') = -1 ) := ");
-            // init template variables
-			cstmt1.setString(2, "user1");
-			cstmt1.setString(3, "a12A345678#");
-			cstmt1.executeUpdate();
-            if (cstmt1.getInt(1) == -1) System.out.println("Passed");
-			else { 
-	        System.out.println("Failed");
-			testCaseResult = -1;
-			}
-			// close connect 
-            con.close();  
+            Connection con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@91.219.60.189:1521/XEPDB1",
+                    args[0], // get login as 1st command line parameter
+                    args[1]); // get password as 2nd command line parameter 
+
+            // create template string with PL/SQL-function "GetTemperature"
+            CallableStatement cstmt1 = con.prepareCall("{? = call GetTemperature(?,?)}");
+            cstmt1.registerOutParameter(1, Types.NUMERIC);
+
+            // Test Case 1
+            System.out.print("TC1 ( 'User is "Anatoliy" and region is "Odessa"' ) := ");
+            cstmt1.setString(2, "Anatoliy");
+            cstmt1.setString(3, "Odessa");
+            cstmt1.executeUpdate();
+
+            // analyze TestCase-result
+            if (cstmt1.getInt(1) == 1) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+                testCaseResult = -1;
+            }
+
+            // Test Case 2
+            System.out.print("TC2 ( 'Empty user and region is "Odessa"' ) := ");
+            cstmt1.setString(2, null);
+            cstmt1.setString(3, "Odessa");
+            cstmt1.executeUpdate();
+
+            // analyze TestCase-result
+            if (cstmt1.getInt(1) == -1) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+                testCaseResult = -1;
+            }
+
+	    // Test Case 3
+            System.out.print("TC3 ( 'User is "Anatoliy" and empty region' ) := ");
+            cstmt1.setString(2, "Anatoliy");
+            cstmt1.setString(3, null);
+            cstmt1.executeUpdate();
+
+            // analyze TestCase-result
+            if (cstmt1.getInt(1) == -1) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+                testCaseResult = -1;
+            }
+
+	    // Test Case 4
+            System.out.print("TC4 ( 'User is "Anatoliy" and region is "Odessa 2+2=4"' ) := ");
+            cstmt1.setString(2, "Anatoliy");
+            cstmt1.setString(3, "Odessa 2+2=4");
+            cstmt1.executeUpdate();
+
+            // analyze TestCase-result
+            if (cstmt1.getInt(1) == -1) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+                testCaseResult = -1;
+            }
+
+	    // Test Case 5
+            System.out.print("TC5 ( 'User is "Anatoliy" and region is "I love chicken with sauce and you"' ) := ");
+            cstmt1.setString(2, "Anatoliy");
+            cstmt1.setString(3, 'I love chicken with sauce and you');
+            cstmt1.executeUpdate();
+
+            // analyze TestCase-result
+            if (cstmt1.getInt(1) == -1) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+                testCaseResult = -1;
+            }
+
+            // close connection
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-	catch(Exception e) { System.out.println(e); }  
-	System.exit(testCaseResult);
-    }    
-}  
+        System.exit(testCaseResult);
+    }
+}
+ 
