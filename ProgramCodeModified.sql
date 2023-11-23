6de34d9405a2baab2c7ae52f6dea2b827a76545f
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2023-11-23 19:02:06 +0100

    Update 3 ProgramCodeModified.sql

--- ProgramCodeModified.sql
+++ ProgramCodeModified.sql
@@ -1,32 +1,29 @@
-CREATE OR REPLACE FUNCTION GetTemperature ()
+CREATE OR REPLACE FUNCTION GetTemperature 
 RETURN INTEGER IS
-    VTemperatureState VARCHAR2 (25);
-    VResult INTEGER := 1;
+    v_temperature_state VARCHAR2(25);
+    v_result INTEGER := 1;
 BEGIN
+    FOR temp_row IN (SELECT w.weatherstate
+                     FROM windy w
+                     WHERE w.region = p_region) 
+    LOOP
+        v_temperature_state := temp_row.weatherstate;
+        EXIT;
+    END LOOP;
 
-  FOR TempRow IN (SELECT W.WHEATHERSTATE
-                   FROM WINDY W
-                   WHERE W.REGION = Region) 
-  LOOP
-    VTemperatureState := TempRow.WHEATHERSTATE;
+    IF p_user_name IS NULL THEN
+        v_result := -1; 
+    ELSIF p_region IS NULL THEN
+        v_result := -1; 
+    ELSIF REGEXP_LIKE(p_region, '[[:digit:][:punct:]]') THEN
+        v_result := -1; 
+    ELSIF LENGTH(p_region) > 25 THEN
+        v_result := -1; 
+    ELSE
+        v_result := 1;
+        DBMS_OUTPUT.PUT_LINE('Today in ' || p_region || ' is a good temperature of ' || v_temperature_state || ' degrees Celsius');
+    END IF;
 
-    EXIT;
-  END LOOP;
-
-  IF UserName IS NULL THEN
-    VResult := -1; 
-  ELSIF Region IS NULL THEN
-    VResult := -1; 
-  ELSIF REGEXP_LIKE(Region, '[[:digit:][:punct:]]') THEN
-    VResult := -1; 
-  ELSIF LENGTH(Region) > 25 THEN
-    VResult := -1; 
-  ELSE
-    VResult := 1;
-    DBMS_OUTPUT.PUT_LINE('Today in ' || Region || ' is a good temperature of ' || VTemperatureState || ' degrees Celsius');
-  END IF;
-
-  RETURN VResult;
-
+    RETURN v_result;
 END GetTemperature;
 /
